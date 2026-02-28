dc287f744b2e61f51cf3da3e625dc53424222f9d
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-02-28 11:51:50 +0000

    Q2 β Values

--- Q2.py
+++ Q2.py
@@ -1,11 +1,11 @@
 #Inputs
 age = int(input("Age : "))
+ethnicity = input("Ethnicity : ")
+location = input("Location : ")
 gender = input("Gender (M/F) : ").upper()
+married = True if input("Married (Y/N) : ").upper() == "Y" else False
 disposableIncome = float(input("Disposable Income / £ : "))
 educationLevel = input("Higher Education Level : ").upper()
-averageBetsPerWeek = float(input("Average number of bets per week : "))
-riskTolerance = float(input("Risk tolerance -5 to 5 (-5 =very low, 5 = very high)"))
-averageValuePerBet = float(input("Average value per best / £ : "))
 
 #Chosen sports
 sports = []
@@ -14,20 +14,32 @@ while(sport != ""):
     sport = input("Betted on sport (Blank to close): ").strip()
     if(sport != ""):
         sports.append(sport)
-    
-
 
 #Calculation
 """Assumptions:
-- We can assume that someone with a higher risk tolerance will be more likely to focus on high-payout low-probability bets
+- women β = -0.13 compared to men
+-  
+
 """
+
+maleBetaDict = {
+    "Never Married " : 0.06,
+    "Single Living w/ S.O." : 0.09,
+    "Seperated" : 0.03,
+    "Divorced" : 0.07,
+    "Widowed" : 0.04,
+    "25-44" : -0.02,
+    "45-64" : -0.14,
+    "65+" : -0.12,
+    "Black" : -0.1,
+    "Hispanic" : -0.06,
+    "Other" : -0.04
+}
 
 expectedOut = 0
 
 #Risk tolerance calculation
-riskToleranceProbabilityCoefficient = 0.05
-riskToleranceValueCoefficient = -1
-expectedOut = (0.5 - riskToleranceProbabilityCoefficient * riskTolerance) * (averageValuePerBet + riskToleranceValueCoefficient * riskTolerance) * averageBetsPerWeek - (1-(0.5 - riskToleranceProbabilityCoefficient * riskTolerance)) * (averageValuePerBet + riskToleranceValueCoefficient * riskTolerance) * averageBetsPerWeek * (365/7)
+
 
 #Output
 print(f"Expected change : {"+" if expectedOut >= 0 else "-"}£{abs(expectedOut)} per year")
