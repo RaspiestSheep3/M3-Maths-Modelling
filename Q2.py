c26ffa2fcafa1fede4704805540f2fd77b71f179
Fatcatcreate <75983504+Fatcatcreate@users.noreply.github.com>
2026-02-28 12:00:00 +0000

    b and beta

--- Q2.py
+++ Q2.py
@@ -21,32 +21,32 @@ while(sport != ""):
 -  
 
 """
-femaleBetaDict = {
-    "Never Married " : -0.02,
-    "Single Living w/ S.O." : 0.01,
-    "Seperated" : -0.09,
-    "Divorced" : -0.05,
-    "Widowed" : 0.01,
-    "25-44" : -0.09,
-    "45-64" : -0.13,
-    "65+" : -0.13,
-    "Black" : 0.02,
-    "Hispanic" : 0.00,
-    "Other" : -0.01
+male_stats_dict = {
+    "Never Married ": {"b": 0.69, "beta": 0.06},
+    "Single Living w/ S.O.": {"b": 2.19, "beta": 0.09},
+    "Seperated": {"b": 1.52, "beta": 0.03},
+    "Divorced": {"b": 1.65, "beta": 0.07},
+    "Widowed": {"b": 1.26, "beta": 0.04},
+    "25-44": {"b": -0.18, "beta": -0.02},
+    "45-64": {"b": -1.58, "beta": -0.14},
+    "65+": {"b": -1.44, "beta": -0.12},
+    "Black": {"b": -1.68, "beta": -0.10},
+    "Hispanic": {"b": -1.22, "beta": -0.06},
+    "Other": {"b": -0.64, "beta": -0.04}
 }
 
-maleBetaDict = {
-    "Never Married " : 0.06,
-    "Single Living w/ S.O." : 0.09,
-    "Seperated" : 0.03,
-    "Divorced" : 0.07,
-    "Widowed" : 0.04,
-    "25-44" : -0.02,
-    "45-64" : -0.14,
-    "65+" : -0.12,
-    "Black" : -0.1,
-    "Hispanic" : -0.06,
-    "Other" : -0.04
+female_stats_dict = {
+    "Never Married ": {"b": -0.16, "beta": -0.02},
+    "Single Living w/ S.O.": {"b": 0.22, "beta": 0.01},
+    "Seperated": {"b": -3.44, "beta": -0.09},
+    "Divorced": {"b": -0.85, "beta": -0.05},
+    "Widowed": {"b": 0.34, "beta": 0.01},
+    "25-44": {"b": -0.92, "beta": -0.09},
+    "45-64": {"b": -1.36, "beta": -0.13},
+    "65+": {"b": -1.99, "beta": -0.13},
+    "Black": {"b": 0.31, "beta": 0.02},
+    "Hispanic": {"b": 0.01, "beta": 0.00},
+    "Other": {"b": -0.10, "beta": -0.01}
 }
 
 expectedOut = 0
